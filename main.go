2a2fba8ecfda6a23f9be26d29ada15d74abc1f47
asep3003 <saepulloha133@gmail.com>
2025-05-19 06:35:14 +0700

    feat: api get station, api get schedule by station

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"github.com/asep3003/go-mrt-schedules/modules/station"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	InitiateRouter()
+}
+
+func InitiateRouter() {
+	var (
+		router = gin.Default()
+		api    = router.Group("/v1/api")
+	)
+
+	station.Initiate(api)
+
+	router.Run(":8000")
 }
---
+++ modules/station/router.go
@@ -0,0 +1,60 @@
+package station
+
+import (
+	"net/http"
+
+	"github.com/asep3003/go-mrt-schedules/common/response"
+	"github.com/gin-gonic/gin"
+)
+
+func Initiate(router *gin.RouterGroup) {
+	stationService := NewService()
+
+	station := router.Group("/stations")
+
+	station.GET("", func(c *gin.Context) {
+		GetAllStation(c, stationService)
+	})
+
+	station.GET("/:id", func(c *gin.Context) {
+		CheckScheduleByStation(c, stationService)
+	})
+}
+
+func GetAllStation(c *gin.Context, service Service) {
+	datas, err := service.GetAllStation()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, response.APIResponse{
+			Success: false,
+			Message: err.Error(),
+			Data:    nil,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, response.APIResponse{
+		Success: true,
+		Message: "Successfully get all stations",
+		Data:    datas,
+	})
+}
+
+func CheckScheduleByStation(c *gin.Context, service Service) {
+	id := c.Param("id")
+
+	datas, err := service.CheckScheduleByStation(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, response.APIResponse{
+			Success: false,
+			Message: err.Error(),
+			Data:    nil,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, response.APIResponse{
+		Success: true,
+		Message: "Successfully get schedules by station",
+		Data:    datas,
+	})
+}
